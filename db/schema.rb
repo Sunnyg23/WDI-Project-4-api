527c347b562b54c40713c6f8930fb39c7e936435
Sunnyg23 <sunbirghag@hotmail.com>
2017-06-01 16:54:58 +0100

    finished backend Auth

---
+++ db/schema.rb
@@ -0,0 +1,16 @@
+
+ActiveRecord::Schema.define(version: 20170601144231) do
+
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
